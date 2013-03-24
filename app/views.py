849388634835eeb23e5ef69d59f93bd25d403872
Depado <hellsing.strelok@gmail.com>
2013-03-24 15:55:26 +0100

    Deleting app/run.py (useless) and adding favicon

--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Sign In', form = form)
