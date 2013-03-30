9fb10c0858c17d85cda616b549c8d3a8e082b60c
Depado <hellsing.strelok@gmail.com>
2013-03-30 18:25:31 +0100

    Added some templates and modified views.py

--- app/views.py
+++ app/views.py
@@ -7,24 +7,24 @@ from forms import LoginForm
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash("That's your password : "+form.password.data)
-        if form.password.data == "azerty":
-        	session['username'] = "admin"
-        	flash("Connected")
-        	return redirect("/index")
-        return redirect("/login")
-    return render_template('login.html', title = 'Sign In', form = form)
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.password.data == "azerty":
+			session['username'] = "admin"
+			flash("You're now logged in as "+session['username'])
+			return redirect("/index")
+		return redirect("/login")
+	return render_template('login.html', title = 'Sign In', form = form)
 
 @app.route('/index')
 def index():
 	if 'username' in session:
-		return render_template('index.html', title = 'intrePid')
+		return render_template('index.html', title = 'IntrePid')
 	else:
-		abort(403)
+		flash("You need to login before you go there.")
+		return redirect("/login")
 
 @app.route('/logout')
 def logout():
-    session.pop('username', None)
-    return redirect("/login")
+	session.pop('username', None)
+	return redirect("/login")
