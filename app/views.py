a3a5cd0170cd61e476383644a0327289e06c3206
Depado <hellsing.strelok@gmail.com>
2013-04-02 10:28:42 +0200

    CPU Load function and attempt of ajax

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@
 from flask import render_template, flash, redirect, session, abort, escape, url_for
 from app import app
 from forms import LoginForm
+from functions import getCpuLoad
 
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/login', methods = ['GET', 'POST'])
@@ -16,7 +17,7 @@ def login():
 		return redirect("/login")
 	return render_template('login.html', title = 'Sign In', form = form)
 
-@app.route('/index')
+@app.route('/index', methods = ['GET','POST'])
 def index():
 	if 'username' in session:
 		return render_template('index.html', title = 'IntrePid')
@@ -24,15 +25,19 @@ def index():
 		flash("You need to login before you go there.")
 		return redirect("/login")
 
-@app.route('/settings')
+@app.route('/stuff', methods= ['GET'])
+def stuff():
+	return getCpuLoad()*100.0
+
+@app.route('/settings', methods = ['GET','POST'])
 def settings():
 	return render_template('settings.html', title = 'IntrePid')
 
-@app.route('/updates')
+@app.route('/updates', methods = ['GET','POST'])
 def updates():
 	return render_template('updates.html', title = 'IntrePid')
 
-@app.route('/scenarios')
+@app.route('/scenarios', methods = ['GET','POST'])
 def scenarios():
 	return render_template('scenarios.html', title = 'IntrePid')
 
